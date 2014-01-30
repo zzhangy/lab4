354e7304af94915b2d2b550504505cfcf902ea20
Yalin Zhang <theyalinzhang@gmail.com>
2014-01-30 10:50:03 -0800

    node.js lab

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
 
@@ -36,6 +37,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
