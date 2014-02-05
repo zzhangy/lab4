7a608247ae59ced81fa381a12b0a09665eeb0419
vagrant <vagrant@precise32.(none)>
2014-02-05 02:12:26 +0000

    trying to debug...

---
+++ routes/project.js
@@ -0,0 +1,6 @@
+exports.viewProject = function(req, res) {
+  // controller code goes here
+  //var name = req.params.name;
+  //console.log("The project name is: " + name);
+  res.render('project');
+};
